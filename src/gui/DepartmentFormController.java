34728e776ab49d445d267fa0e9a91410e4ed32bb
Thaynan Oliveira <thaynan-rodrigues@outlook.com>
2021-08-01 15:59:55 -0300

    Ajustando entrada de dados

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,10 +9,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 
 //Controlar os componentes da tela de cadastro de departamentos
 public class DepartmentFormController implements Initializable{
+	
+	private Department entity;
 	
 	@FXML
 	private TextField txtId;
@@ -28,6 +31,10 @@ public class DepartmentFormController implements Initializable{
 	
 	@FXML 
 	private Button btCancel;
+	
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
 	
 	@FXML
 	public void onBtSaveAction() {
@@ -48,6 +55,13 @@ public class DepartmentFormController implements Initializable{
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 30);
 	}	
-	
+	public void updateFormData() {
+		
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
 
 }
