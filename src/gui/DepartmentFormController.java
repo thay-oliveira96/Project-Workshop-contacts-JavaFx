905f231aff5f986d66d47e49c81bb29c907acf40
Thaynan Oliveira <thaynan-rodrigues@outlook.com>
2021-08-01 15:44:43 -0300

    Implementação de Controle em Formularios

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,53 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+//Controlar os componentes da tela de cadastro de departamentos
+public class DepartmentFormController implements Initializable{
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML 
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML 
+	private Button btCancel;
+	
+	@FXML
+	public void onBtSaveAction() {
+		System.out.println("OnBtSaveAction");
+	}
+	@FXML
+	public void onBtCancelAction() {
+		System.out.println("OnBtCancelAction");
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		initializeNodes();
+		
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}	
+	
+
+}
