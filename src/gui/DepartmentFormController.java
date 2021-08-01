4fa3425831ee9ea78df5ace4826a90697125cc28
Thaynan Oliveira <thaynan-rodrigues@outlook.com>
2021-08-01 16:44:57 -0300

    Implementação do Botão e metodos salvar formulario departamento

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,19 +3,27 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 
 //Controlar os componentes da tela de cadastro de departamentos
 public class DepartmentFormController implements Initializable{
 	
 	private Department entity;
+	
+	private DepartmentService service;
 	
 	@FXML
 	private TextField txtId;
@@ -35,14 +43,39 @@ public class DepartmentFormController implements Initializable{
 	public void setDepartment(Department entity) {
 		this.entity = entity;
 	}
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
 	
-	@FXML
-	public void onBtSaveAction() {
-		System.out.println("OnBtSaveAction");
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		
+		return obj;
 	}
 	@FXML
-	public void onBtCancelAction() {
-		System.out.println("OnBtCancelAction");
+	public void onBtSaveAction(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			utils.currentStage(event).close();
+		}
+		catch(DbException e) {
+			Alerts.showAlert("Error saving Object", null, e.getMessage(), AlertType.ERROR);
+			
+		}
+	}
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		utils.currentStage(event).close();
 	}
 
 	@Override
